fcdc46a753cb9729d7080bfa7b35a21ab04b54ce
Asus TUF <honzahoangminhquan1234@gmail.com>
2025-11-17 19:35:35 +0100

    made web app responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,16 +8,16 @@ import pikachuGif from "/images/pikachu-shocked-face-stunned.gif";
 
 export default function Header() {
     return (
-        <div className="header container mx-xl mx-auto flex justify-between items-center py-10">
+        <div className="header container mx-xl mx-auto flex justify-between items-center py-10 max-sm:max-w-[80vw]">
             <div className="header_logo">
                 <Link to="/">
-                    <h2 className="font-[Poppins] font-bold text-4xl">
+                    <h2 className="font-[Poppins] font-bold text-2xl sm:text-4xl ">
                         Fortune
                         <span className="text-gold">Cracker</span>
                     </h2>
                 </Link>
             </div>
-            <div className="header__actions flex items-center gap-5">
+            <div className="header__actions flex items-center gap-5 hidden sm:flex">
                 <Button text={"Open"} link={"/"} />
                 <div className="header__user">
                     <img 
