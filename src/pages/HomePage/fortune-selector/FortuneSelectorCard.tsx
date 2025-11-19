a51873d3088b7ffbbf57b66f4a8ff85c9c1e2c27
Asus TUF <honzahoangminhquan1234@gmail.com>
2025-11-19 15:41:53 +0100

    made mode selectors and history cards unselectable

--- src/pages/HomePage/fortune-selector/FortuneSelectorCard.tsx
+++ src/pages/HomePage/fortune-selector/FortuneSelectorCard.tsx
@@ -24,7 +24,7 @@ export default function FortuneSelectorCard({ name, authors, mode }: FortuneSele
 
     return (
         <div
-            className="fortune__selector__card flex flex-col justify-center items-center px-5 py-3 rounded-[20px] bg-blackSecondary font-[Rajdhani] shadow-lg cursor-pointer transition-all ease-in-out duration-200 hover:shadow-(--gold-glow) hover:bg-darkGold hover:text-neutral-950 md:px-10 md:py-5 md:rounded-[25px] md:min-w-[230px]"
+            className="fortune__selector__card flex flex-col justify-center items-center px-5 py-3 rounded-[20px] bg-blackSecondary font-[Rajdhani] shadow-lg cursor-pointer transition-all ease-in-out duration-200 select-none hover:shadow-(--gold-glow) hover:bg-darkGold hover:text-neutral-950 md:px-10 md:py-5 md:rounded-[25px] md:min-w-[230px]"
             data-mode={mode}
             onClick={() => setSelectedMode(mode)}    
             ref={fortuneSelectorCard}
--- src/pages/HomePage/history/HistoryCard.tsx
+++ src/pages/HomePage/history/HistoryCard.tsx
@@ -14,7 +14,7 @@ export default function HistoryCard({ fortune }: HistoryCardProps) {
 
     return (
         <div 
-            className="history__card relative flex justify-center items-center bg-blackSecondary w-full h-17 max-h-17 rounded-[20px] shadow-lg cursor-pointer transition-all duration-200 ease-in-out hover:bg-darkGold sm:w-40 sm:max-w-40"
+            className="history__card relative flex justify-center items-center bg-blackSecondary w-full h-17 max-h-17 rounded-[20px] shadow-lg cursor-pointer transition-all duration-200 ease-in-out select-none hover:bg-darkGold sm:w-40 sm:max-w-40"
             onClick={() => { setCurrentFortune(fortune); setIsCracked(true) }}
         >
             <div className="history__card__count absolute -top-3 bg-darkGray px-3 py-[3px] rounded-xl text-base text-darkGold font-[Rajdhani] font-medium hover:text-inherit">
